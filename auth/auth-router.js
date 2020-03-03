16c20653adcb90569728e05491b7740eed6da016
Brennon Nuckols <brennuck@gmail.com>
2020-03-03 14:39:37 -0700

    day 2 mvp

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -37,4 +37,18 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ you: "can check out any time you like, but you can never leave" })
+      } else {
+        res.status(200).json({ you: "logged out successfully" })
+      }
+    })
+  } else {
+    res.status(200).json({ bye: "felicia" })
+  }
+})
+
 module.exports = router;
